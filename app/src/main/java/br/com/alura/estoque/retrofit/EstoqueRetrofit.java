e25fc5c55bb5bbde01b3e7b6ca612f051e63df29
Robson Lima <rsl.robson@gmail.com>
2020-05-11 14:37:55 -0300

    Adicionando o logging interceptor

--- app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
+++ app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
@@ -1,6 +1,8 @@
 package br.com.alura.estoque.retrofit;
 
 import br.com.alura.estoque.retrofit.service.ProdutoService;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,8 +11,15 @@ public class EstoqueRetrofit {
     private final ProdutoService produtoService;
 
     public EstoqueRetrofit() {
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://192.168.0.143:8080/")
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
