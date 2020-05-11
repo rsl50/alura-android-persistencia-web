0a94f94da0e497a715b15f89dee611b76759989d
Robson Lima <rsl.robson@gmail.com>
2020-05-11 14:02:00 -0300

    Buscando os produtos da API

--- app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
+++ app/src/main/java/br/com/alura/estoque/retrofit/EstoqueRetrofit.java
@@ -2,6 +2,7 @@ package br.com.alura.estoque.retrofit;
 
 import br.com.alura.estoque.retrofit.service.ProdutoService;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class EstoqueRetrofit {
 
@@ -10,6 +11,7 @@ public class EstoqueRetrofit {
     public EstoqueRetrofit() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://192.168.0.143:8080/")
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         produtoService = retrofit.create(ProdutoService.class);
